ccf5056eee2d61ace8c4f44d2fda65f8bf62b50f
varsha-316 <varsha160530@gmail.com>
2022-08-12 21:07:14 +0530

    commit

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 
+window.addEventListener("load", () => {
+    document.querySelector(".main").classList.remove("hidden");
+    document.querySelector(".home-section").classList.add("active");
+    /**page loader */
+    document.querySelector(".page-loader").classList.add("fade-out");
+    setTimeout( () => {
+        document.querySelector(".page-loader").style.display = "none";
+    },600);
+});
+
+
+
 //header toogle navbar
 const navtoggler =  document.querySelector(".nav-toggler");
 navtoggler.addEventListener("click", () => {
